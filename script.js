a9db559d764b813b8e351fa0a16f0d8ab2a85f80
marilia <marilia.pacifico@gmail.com>
2021-09-01 17:37:46 -0300

    add highscore config

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -21,6 +22,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   }
 
   // when the guess is higher than the secret number
