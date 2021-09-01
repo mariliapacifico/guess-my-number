2fa1ce06586ad8b556c25c2b71df074844cbf0f4
marilia <marilia.pacifico@gmail.com>
2021-09-01 12:23:01 -0300

    update CSS styles

--- script.js
+++ script.js
@@ -8,20 +8,34 @@ document.querySelector(".check").addEventListener("click", function() {
     const guess = Number (document.querySelector(".guess").value);
     console.log(guess, typeof guess);
 
+// when there's no input 
     if (!guess) {
         document.querySelector(".message").textContent = "⛔ No Number!";
-    } else if (guess === secretNumber) {
-        document.querySelector(".message").textContent = "🎉 Correct Number!"
-    } else if (guess > secretNumber) {
+    }
+
+// when player wins
+    else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "🎉 Correct Number!";
+         
+        document.querySelector("body").style.backgroundColor = "#60b347";
+
+        document.querySelector(".number").style.width = "30rem";
+    } 
+
+// when the guess is higher than the secret number
+    else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector(".message").textContent = "📈 Too High...";
         score--;
         document.querySelector(".score").textContent = score;
-        } else {
+        }  else {
             document.querySelector(".message").textContent = "💥 You lost the game...";
         document.querySelector(".score").textContent = 0;
         }
-    } else if (guess < secretNumber) {
+    } 
+
+// when the guess is lower than the secret number
+    else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector(".message").textContent = "📉 Too Low...";
         score--;
